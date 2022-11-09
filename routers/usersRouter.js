3b7ee199bc0eda4bddd113fa003e58aab45b28bc
manuelJung <manuel.jung.wwi12@gmail.com>
2022-11-09 16:08:18 +0100

    task 2

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import * as controller from '../controllers/usersController.js'
+import * as validations from '../lib/validations/usersValidation.js'
+import validate from '../lib/middlewares/validate.js'
 
 const app = express.Router()
 
 app.get('/', controller.getUser)
-app.post('/register', controller.register)
-app.post('/login', controller.login)
+app.post('/register', validate(validations.register), controller.register)
+app.post('/login', validate(validations.login), controller.login)
 
 export default app
