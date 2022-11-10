7dc547e20fdbf4935f80206ba4de21c29ceeca14
manuelJung <manuel.jung.wwi12@gmail.com>
2022-11-10 16:12:17 +0100

    task 6

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -10,4 +10,6 @@ app.get('/', auth, controller.getUser)
 app.post('/register', validate(validations.register), controller.register)
 app.post('/login', validate(validations.login), controller.login)
 
+app.post('/change-password', auth, validate(validations.changePassword))
+
 export default app
